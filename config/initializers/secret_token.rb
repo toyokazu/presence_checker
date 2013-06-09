9af431691e4ab627c5af1cd0cf2bb4266d2ac4f4
Toyokazu Akiyama <toyokazu@gmail.com>
2013-06-09 11:18:54 +0900

    remove secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-PresenceChecker::Application.config.secret_token = 'c6b645fff9d7e58fdca3a0ab050768a9f9ad52ae24a439c26ddcb190ec61c1ae4a988e161163b9d5e18b769ec2eaa10243177cad5d09676137c256e64e2b4562'
